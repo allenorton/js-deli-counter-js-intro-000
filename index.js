c93b21184467e28fff80278e594294e7bead918e
Allene Norton <norton.allene@gmail.com>
2020-05-23 03:16:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 function takeANumber (line, name){
-  line.push(`${name}`)
+  line.push(name)
   return (`Welcome, ${name}. You are number ${line.length} in line.`)
 }
 function nowServing (line) {
-  let x = 0
-  while (x < line.length) {
-    x++
-  }
-  if (line.length === 0) {
-    return "There is nobody waiting to be served!"
-  }
-  else {
-    return `Currently serving ${line.shift()}.`
-  }
+  return line.length === 0 ? "There is nobody waiting to be served!" : `Currently serving ${line.shift()}.`
 }
 function currentLine(line) {
   let x = 0
   while (x < line.length){
-    line.push(`` + [x+1] + `.` + line[x]); x++
+    line.push((x+1) + `.` + line[x]); x++
   }
   if (line.length === 0){
     return ("The line is currently empty.")
